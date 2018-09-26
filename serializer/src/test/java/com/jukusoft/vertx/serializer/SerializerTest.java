b5187631595a4adc5ed1c26d65b95b79c25d9b12
juku <kuenzel.justin@t-online.de>
2018-09-26 17:11:49 +0200

    added little benchmark to junit tests.

--- serializer/src/test/java/com/jukusoft/vertx/serializer/SerializerTest.java
+++ serializer/src/test/java/com/jukusoft/vertx/serializer/SerializerTest.java
@@ -26,10 +26,32 @@ public class SerializerTest {
     public void testSerializer () {
         TestObject obj = new TestObject();
 
+        long startTime = System.currentTimeMillis();
+
         Buffer buffer = Serializer.serialize(obj);
         TestObject obj1 = Serializer.unserialize(buffer, TestObject.class);
 
+        long endTime = System.currentTimeMillis();
+        long timeDiff = endTime - startTime;
+        System.out.println("[Benchmark] time needed for serialization of one object: " + timeDiff + "ms");
+
         assertEquals(obj.getClass(), obj1.getClass());
+    }
+
+    @Test
+    public void testSerialize1000Objects () {
+        TestObject obj = new TestObject();
+
+        long startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < 1000; i++) {
+            Buffer buffer = Serializer.serialize(obj);
+            TestObject obj1 = Serializer.unserialize(buffer, TestObject.class);
+        }
+
+        long endTime = System.currentTimeMillis();
+        long timeDiff = endTime - startTime;
+        System.out.println("[Benchmark] time needed for serialization of 1000 objects: " + timeDiff + "ms");
     }
 
 }
