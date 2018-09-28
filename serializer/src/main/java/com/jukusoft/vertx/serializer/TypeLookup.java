888d8a38f22561fd9d1a1fe852f7b0b1ebc78933
juku <kuenzel.justin@t-online.de>
2018-09-29 00:07:31 +0200

    added benchmark module.

--- serializer/src/main/java/com/jukusoft/vertx/serializer/TypeLookup.java
+++ serializer/src/main/java/com/jukusoft/vertx/serializer/TypeLookup.java
@@ -1,7 +1,30 @@
 package com.jukusoft.vertx.serializer;
 
+import com.carrotsearch.hppc.IntObjectHashMap;
+import com.carrotsearch.hppc.IntObjectMap;
+
 public class TypeLookup {
 
-    //
+    protected static IntObjectMap<Class<? extends SerializableObject>> map = new IntObjectHashMap<>();
+
+    public static void register (byte type, byte extendedType, Class<? extends SerializableObject> cls) {
+        int key = bytesToInt(type, extendedType);
+        map.put(key, cls);
+    }
+
+    public static void unregister (byte type, byte extendedType) {
+        int key = bytesToInt(type, extendedType);
+        map.remove(key);
+    }
+
+    public static Class<? extends SerializableObject> find (byte type, byte extendedType) {
+        int key = bytesToInt(type, extendedType);
+        return map.get(key);
+    }
+
+    protected static int bytesToInt (byte type, byte extendedType) {
+        int val = ((type & 0xff) << 8) | (extendedType & 0xff);
+        return val;
+    }
 
 }
