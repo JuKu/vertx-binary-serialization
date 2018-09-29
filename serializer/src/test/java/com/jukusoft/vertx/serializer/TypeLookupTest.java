52d68e8d1f439c8dbe82153c382591a1e561b506
juku <kuenzel.justin@t-online.de>
2018-09-29 03:37:44 +0200

    added some junit tests for class TypeLookup.

---
+++ serializer/src/test/java/com/jukusoft/vertx/serializer/TypeLookupTest.java
@@ -0,0 +1,12 @@
+package com.jukusoft.vertx.serializer;
+
+import org.junit.Test;
+
+public class TypeLookupTest {
+
+    @Test
+    public void testConstructor () {
+        new TypeLookup();
+    }
+
+}
