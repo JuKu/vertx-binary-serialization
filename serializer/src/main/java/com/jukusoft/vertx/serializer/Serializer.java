8d5cf5212f509c7a2c6b717f52dfc173fdd55b48
juku <kuenzel.justin@t-online.de>
2018-09-30 17:00:55 +0200

    changed class Serializer.

--- serializer/src/main/java/com/jukusoft/vertx/serializer/Serializer.java
+++ serializer/src/main/java/com/jukusoft/vertx/serializer/Serializer.java
@@ -17,7 +17,7 @@ public class Serializer {
         //
     }
 
-    public static Buffer serialize (SerializableObject obj) {
+    public static final Buffer serialize (SerializableObject obj) {
         Buffer buf = Buffer.buffer();
 
         if (obj.getClass().getAnnotation(MessageType.class) == null) {
@@ -50,103 +50,107 @@ public class Serializer {
         _pos += 2;
 
         try {
-            //iterate through all fields in class
-            for (Field field : obj.getClass().getDeclaredFields()) {
-                field.setAccessible(true);
-
-                //iterate through all annotations for this field
-                for (Annotation annotation : field.getAnnotations()) {
-                    Class<? extends Annotation> clazz = annotation.annotationType();
-
-                    if (clazz == SInteger.class) {
-                        //get integer value of field
-                        int value = field.getInt(obj);
-
-                        //check range
-
-                        //add to protocol
-                        buf.setInt(_pos, value);
-                        _pos += 4;
-                    } else if (clazz == SString.class) {
-                        String value = (String) field.get(obj);
-
-                        //check max characters
-
-                        //add length of string
-                        buf.setInt(_pos, value.length());
-                        _pos += 4;
-
-                        //add string
-                        buf.setString(_pos, value);
-                        _pos += value.length() * 4;
-                    } else if (clazz == SBoolean.class) {
-                        boolean bool = field.getBoolean(obj);
-
-                        //add to protocol
-                        buf.setByte(_pos, (byte) (bool ? 0x01 : 0x02));
-                        _pos += 1;
-                    } else if (clazz == SChar.class) {
-                        char character = field.getChar(obj);
-
-                        //add to protocol
-                        buf.setShort(_pos, (short) character);
-                        _pos += 2;
-                    } else if (clazz == SByte.class) {
-                        byte b = field.getByte(obj);
-
-                        //add to protocol
-                        buf.setByte(_pos, b);
-                        _pos += 1;
-                    } else if (clazz == SBytes.class) {
-                        byte[] bytes = (byte[]) field.get(obj);
-
-                        if (bytes == null) {
-                            throw new NullPointerException("byte array cannot be null in field '" + field.getName() + "' in class '" + clazz.getCanonicalName() + "'!");
-                        }
-
-                        /*if (bytes.length > 4294967296l) {
-                            throw new SerializerException("max 4294967296 bytes are allowed in array in field '" + field.getName() + "' class '" + clazz.getCanonicalName() + "'!");
-                        }*/
-
-                        //add to protocol
-                        buf.setUnsignedInt(_pos, bytes.length);
-                        _pos += 4;
-
-                        buf.setBytes(_pos, bytes);
-                        _pos += bytes.length;
-                    } else if (clazz == SShort.class) {
-                        short s = field.getShort(obj);
-
-                        //add to protocol
-                        buf.setShort(_pos, s);
-                        _pos += 2;
-                    } else if (clazz == SLong.class) {
-                        long l = field.getLong(obj);
-
-                        //add to protocol
-                        buf.setLong(_pos, l);
-                        _pos += 8;
-                    } else if (clazz == SFloat.class) {
-                        float floatValue = field.getFloat(obj);
-
-                        //add to protocol
-                        buf.setFloat(_pos, floatValue);
-                        _pos += 4;
-                    } else if (clazz == SDouble.class) {
-                        double d = field.getDouble(obj);
-
-                        //add to protocol
-                        buf.setDouble(_pos, d);
-                        _pos += 8;
-                    }
-                }
-            }
+            serializeFields(buf, obj, _pos);
         } catch (IllegalAccessException e) {
             e.printStackTrace();
             throw new SerializerException("Cannot access field in class " + obj.getClass().getCanonicalName() + "!");
         }
 
         return buf;
+    }
+
+    protected static final void serializeFields (Buffer buf, SerializableObject obj, int _pos) throws IllegalAccessException {
+        //iterate through all fields in class
+        for (Field field : obj.getClass().getDeclaredFields()) {
+            field.setAccessible(true);
+
+            //iterate through all annotations for this field
+            for (Annotation annotation : field.getAnnotations()) {
+                Class<? extends Annotation> clazz = annotation.annotationType();
+
+                if (clazz == SInteger.class) {
+                    //get integer value of field
+                    int value = field.getInt(obj);
+
+                    //check range
+
+                    //add to protocol
+                    buf.setInt(_pos, value);
+                    _pos += 4;
+                } else if (clazz == SString.class) {
+                    String value = (String) field.get(obj);
+
+                    //check max characters
+
+                    //add length of string
+                    buf.setInt(_pos, value.length());
+                    _pos += 4;
+
+                    //add string
+                    buf.setString(_pos, value);
+                    _pos += value.length() * 4;
+                } else if (clazz == SBoolean.class) {
+                    boolean bool = field.getBoolean(obj);
+
+                    //add to protocol
+                    buf.setByte(_pos, (byte) (bool ? 0x01 : 0x02));
+                    _pos += 1;
+                } else if (clazz == SChar.class) {
+                    char character = field.getChar(obj);
+
+                    //add to protocol
+                    buf.setShort(_pos, (short) character);
+                    _pos += 2;
+                } else if (clazz == SByte.class) {
+                    byte b = field.getByte(obj);
+
+                    //add to protocol
+                    buf.setByte(_pos, b);
+                    _pos += 1;
+                } else if (clazz == SBytes.class) {
+                    byte[] bytes = (byte[]) field.get(obj);
+
+                    if (bytes == null) {
+                        throw new NullPointerException("byte array cannot be null in field '" + field.getName() + "' in class '" + clazz.getCanonicalName() + "'!");
+                    }
+
+                        /*if (bytes.length > 4294967296l) {
+                            throw new SerializerException("max 4294967296 bytes are allowed in array in field '" + field.getName() + "' class '" + clazz.getCanonicalName() + "'!");
+                        }*/
+
+                    //add to protocol
+                    buf.setUnsignedInt(_pos, bytes.length);
+                    _pos += 4;
+
+                    buf.setBytes(_pos, bytes);
+                    _pos += bytes.length;
+                } else if (clazz == SShort.class) {
+                    short s = field.getShort(obj);
+
+                    //add to protocol
+                    buf.setShort(_pos, s);
+                    _pos += 2;
+                } else if (clazz == SLong.class) {
+                    long l = field.getLong(obj);
+
+                    //add to protocol
+                    buf.setLong(_pos, l);
+                    _pos += 8;
+                } else if (clazz == SFloat.class) {
+                    float floatValue = field.getFloat(obj);
+
+                    //add to protocol
+                    buf.setFloat(_pos, floatValue);
+                    _pos += 4;
+                } else if (clazz == SDouble.class) {
+                    double d = field.getDouble(obj);
+
+                    //add to protocol
+                    buf.setDouble(_pos, d);
+                    _pos += 8;
+                }
+            }
+        }
     }
 
     public static <T extends SerializableObject> T unserialize (Buffer msg, Class<T> cls, int _pos) {
