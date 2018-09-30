744b38e98ef3f135224baa47e748bb6d90be98d9
juku <kuenzel.justin@t-online.de>
2018-09-30 18:30:40 +0200

    added class SocketUtils.

---
+++ serializer/src/test/java/com/jukusoft/vertx/serializer/utils/SocketUtilsTest.java
@@ -0,0 +1,33 @@
+package com.jukusoft.vertx.serializer.utils;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+import static org.junit.Assert.assertEquals;
+
+public class SocketUtilsTest {
+
+    @Test
+    public void testConstructor () {
+        new SocketUtils();
+    }
+
+    @Test
+    public void testCheckRemoteTCPPort () throws IOException {
+        assertEquals(false, SocketUtils.checkRemoteTCPPort("127.0.0.1", 10000, 500));
+    }
+
+    /*@Test
+    public void testCheckRemoteTCPPort1 () throws IOException {
+        assertEquals(true, SocketUtils.checkRemoteTCPPort("jukusoft.com", 80, 500));
+    }*/
+
+    @Test
+    public void testListOwnIPs () throws SocketException, UnknownHostException {
+        assertEquals(true, SocketUtils.listOwnIPs().size() > 0);
+    }
+
+}
