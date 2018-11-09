b3e74b062851612833e2ee02e8079517d9aba2e5
juku <kuenzel.justin@t-online.de>
2018-11-09 23:31:49 +0100

    changed annotation SBytes, added javadocs.

--- serializer/src/main/java/com/jukusoft/vertx/serializer/annotations/SBytes.java
+++ serializer/src/main/java/com/jukusoft/vertx/serializer/annotations/SBytes.java
@@ -5,6 +5,15 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+* annotation used to serialize byte arrays
+ * for example:
+ *
+ * <pre>
+ * @SBytes
+ * public byte[] bytes = new byte[0];
+ * </pre>
+*/
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface SBytes {
